10aec64204a30b9704b8751d912012f315b5a5ec
Simeon <simeonogunyinka@gmail.com>
2025-06-22 08:05:29 +0100

    Made mobile text smaller

--- src/timetable.jsx
+++ src/timetable.jsx
@@ -9,7 +9,7 @@ const timetable = () => {
         Computer Engineering HND1
       </h2>
       <table className="w-full border-collapse border border-gray-300 text-sm">
-        <thead className="bg-gray-100">
+        <thead className="bg-gray-100 text-xs">
           <tr>
             <th className="border p-2">Day</th>
             <th className="border p-2">8 – 10</th>
@@ -21,30 +21,30 @@ const timetable = () => {
         <tbody>
           {data.map((row, index) => (
             <tr key={index}>
-              <td className="border p-2">{row.day}</td>
-              <td className="border p-2 whitespace-pre-wrap">
-                <div className='text-center'>
+              <td className="border p-2 text-center">{row.day}</td>
+              <td className="border p-2 ">
+                <div className='text-center text-xs'>
                     <div>{row.Period_1.course}</div>
                     <div className='font-bold'>{row.Period_1.venue}</div>
                     <div>{row.Period_1.lecturer}</div>
                 </div>
               </td>
               <td className="border p-2 whitespace-pre-wrap">
-                <div className='text-center'>
+                <div className='text-center text-xs'>
                     <div>{row.Period_2.course}</div>
                     <div className='font-bold'>{row.Period_2.venue}</div>
                     <div>{row.Period_2.lecturer}</div>
                 </div>
               </td>
               <td className="border p-2 whitespace-pre-wrap">
-                <div className='text-center'>
+                <div className='text-center text-xs'>
                     <div>{row.Period_3.course}</div>
                     <div className='font-bold'>{row.Period_3.venue}</div>
                     <div>{row.Period_3.lecturer}</div>
                 </div>
               </td>
               <td className="border p-2 whitespace-pre-wrap">
-                <div className='text-center'>
+                <div className='text-center text-xs'>
                     <div>{row.Period_4.course}</div>
                     <div className='font-bold'>{row.Period_4.venue}</div>
                     <div>{row.Period_4.lecturer}</div>
