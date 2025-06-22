0c37baa6bd0a4bdefb8934eebf6456094c7e07f7
Simeon <simeonogunyinka@gmail.com>
2025-06-22 12:51:58 +0100

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from "react-ga4";
 import Home from './pages/home';
 import ClassPage from './pages/classPage';
 import Footer from './components/footer';
 
 function App() {
+  useEffect(() => {
+  ReactGA.initialize("G-CL9BD7CD9J");
+  ReactGA.send("pageview");
+}, []);
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
