9682f6acc3a5844c4315789d06d16a81c49e63db
Simeon <simeonogunyinka@gmail.com>
2025-07-23 09:12:56 +0100

    added pwa and icons

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
+import icons from 'public/icons.json'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Timetable App",
+        short_name: "Timetable",
+        description: "A smart timetable management PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: icons,
+      },
+    }),
   ],
-  
-})
+});
